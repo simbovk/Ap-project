f11c9e104ac65d8ef4b0a19a746b368cda84df53
Amirali Vakili <amirv1382@gmail.com>
2022-06-04 19:02:33 +0430

    cart page updated

--- lib/ShoppingCartPage.dart
+++ lib/ShoppingCartPage.dart
@@ -13,18 +13,33 @@ class  ShoppingCartState extends State<ShoppingCart>{
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-         appBar: AppBar(
-            title: const Text('Tabbed AppBar'),
-            bottom: TabBar(
-              isScrollable: true,
-              tabs: [
-                // Text('Shopping Cart'),
-                // Text(data)
-              ],
-            ),
-          ),
-          
+        body: DefaultTabController(
+                length: 2,
+                initialIndex: 0,
+                child: Column(
+                  children: [
+                    Container(
+                      child: TabBar(
+                          labelColor: Colors.purple,
+                          unselectedLabelColor: Colors.white,
+                          tabs: [
+                            Tab(child: Text("Posts")),
+                            Tab(child: Text("About")),
+                          ]),
+                    ),
+                    SizedBox(
+                      height: 400,
+                      child: TabBarView(
+                        children: <Widget>[
+                         
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
       ),
     );
   }
